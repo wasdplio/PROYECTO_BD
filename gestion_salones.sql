4d68439a5816e7c593e29013d41417d4e3cabdbf
wasdplio <wasdplio71@gmail.com>
2025-06-08 19:41:44 -0500

    Add files via upload

---
+++ gestion_salones.sql
@@ -0,0 +1,266 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 09-06-2025 a las 02:39:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestion_salones`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `computadores`
+--
+
+CREATE TABLE `computadores` (
+  `id` int(11) NOT NULL,
+  `salon_id` int(11) NOT NULL,
+  `codigo_patrimonio` varchar(50) DEFAULT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `sistema_operativo` varchar(100) DEFAULT NULL,
+  `ram_gb` int(11) DEFAULT NULL,
+  `almacenamiento_gb` int(11) DEFAULT NULL,
+  `tipo_almacenamiento` enum('HDD','SSD','NVMe') DEFAULT NULL,
+  `estado` enum('operativo','mantenimiento','dañado','retirado') DEFAULT 'operativo',
+  `fecha_instalacion` date DEFAULT NULL,
+  `ultimo_mantenimiento` date DEFAULT NULL,
+  `observaciones` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `incidencias`
+--
+
+CREATE TABLE `incidencias` (
+  `id` int(11) NOT NULL,
+  `computador_id` int(11) NOT NULL,
+  `usuario_reporte_id` int(11) NOT NULL,
+  `asignado_nombre` varchar(100) DEFAULT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `estado` enum('pendiente','asignado','en_proceso','resuelto') DEFAULT 'pendiente',
+  `prioridad` enum('baja','media','alta','critica') DEFAULT 'media',
+  `fecha_reporte` date DEFAULT NULL,
+  `fecha_asignacion` date DEFAULT NULL,
+  `fecha_resolucion` date DEFAULT NULL,
+  `solucion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reparaciones`
+--
+
+CREATE TABLE `reparaciones` (
+  `id` int(11) NOT NULL,
+  `computadora_id` int(11) NOT NULL,
+  `fecha_reparacion` date NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha_completada` date DEFAULT NULL,
+  `persona_reporto` varchar(100) DEFAULT NULL,
+  `persona_realizo` varchar(100) DEFAULT NULL,
+  `solucion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salones`
+--
+
+CREATE TABLE `salones` (
+  `id` int(11) NOT NULL,
+  `sede_id` int(11) NOT NULL,
+  `codigo_salon` varchar(20) NOT NULL,
+  `piso` int(11) NOT NULL,
+  `capacidad` int(11) DEFAULT NULL,
+  `numero_computadores` int(11) DEFAULT 0,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `salones`
+--
+
+INSERT INTO `salones` (`id`, `sede_id`, `codigo_salon`, `piso`, `capacidad`, `numero_computadores`, `descripcion`) VALUES
+(39, 12, 'A101', 9, 10, 13, 'aaa');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sedes`
+--
+
+CREATE TABLE `sedes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `responsable` varchar(100) DEFAULT NULL,
+  `activa` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sedes`
+--
+
+INSERT INTO `sedes` (`id`, `nombre`, `direccion`, `telefono`, `responsable`, `activa`) VALUES
+(12, 'SEDE PRINCIPAL', 'calle 1 av 0 ', '3118122927', 'Kevin Ortiz', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `rol` enum('admin','tecnico','usuario') DEFAULT 'usuario',
+  `fecha_registro` datetime DEFAULT current_timestamp(),
+  `ultimo_login` datetime DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT 1,
+  `contraseña` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `computadores`
+--
+ALTER TABLE `computadores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo_patrimonio` (`codigo_patrimonio`),
+  ADD KEY `salon_id` (`salon_id`);
+
+--
+-- Indices de la tabla `incidencias`
+--
+ALTER TABLE `incidencias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `computador_id` (`computador_id`),
+  ADD KEY `usuario_reporte_id` (`usuario_reporte_id`);
+
+--
+-- Indices de la tabla `reparaciones`
+--
+ALTER TABLE `reparaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `computadora_id` (`computadora_id`);
+
+--
+-- Indices de la tabla `salones`
+--
+ALTER TABLE `salones`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sede_id` (`sede_id`,`codigo_salon`);
+
+--
+-- Indices de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `computadores`
+--
+ALTER TABLE `computadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `incidencias`
+--
+ALTER TABLE `incidencias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `reparaciones`
+--
+ALTER TABLE `reparaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `salones`
+--
+ALTER TABLE `salones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `computadores`
+--
+ALTER TABLE `computadores`
+  ADD CONSTRAINT `computadores_ibfk_1` FOREIGN KEY (`salon_id`) REFERENCES `salones` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `incidencias`
+--
+ALTER TABLE `incidencias`
+  ADD CONSTRAINT `incidencias_ibfk_1` FOREIGN KEY (`computador_id`) REFERENCES `computadores` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `incidencias_ibfk_2` FOREIGN KEY (`usuario_reporte_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `reparaciones`
+--
+ALTER TABLE `reparaciones`
+  ADD CONSTRAINT `reparaciones_ibfk_1` FOREIGN KEY (`computadora_id`) REFERENCES `computadores` (`id`);
+
+--
+-- Filtros para la tabla `salones`
+--
+ALTER TABLE `salones`
+  ADD CONSTRAINT `salones_ibfk_1` FOREIGN KEY (`sede_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
